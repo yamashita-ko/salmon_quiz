d97691fd4402e935f8cd4038c44095933acd02c2
yamashita.k <yamashita.kohei1103@gmail.com>
2024-01-24 05:56:48 +0900

    ファイル名を変更

--- src/main/java/com/example/demo/dao/WeaponMasterDao.java
+++ src/main/java/com/example/demo/dao/WeaponMasterDao.java
@@ -5,34 +5,34 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.demo.bean.WeaponMasterBean;
+import com.example.demo.dto.WeaponMasterDto;
 
 public class WeaponMasterDao extends BaseDao{
-	public List<WeaponMasterBean> findAll() {
+	public List<WeaponMasterDto> findAll() {
 		Statement stmt = null;
 		ResultSet rs = null;
-		List<WeaponMasterBean> result = new ArrayList<>();
+		List<WeaponMasterDto> result = new ArrayList<>();
 		String sql = "SELECT * FROM weapon_master INNER JOIN weapon_image on weapon_master.id = weapon_image.id INNER JOIN weapon_type_master on weapon_master.type = weapon_type_master.id";
 		try {
 			connect();
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(sql);
 			while(rs.next()) {
-				WeaponMasterBean bean = new WeaponMasterBean();
-				bean.setId(rs.getInt("id"));
-				bean.setName(rs.getString("weapon_master.name"));
-				bean.setType(rs.getString("weapon_type_master.name"));
-				bean.setMaxDamage(rs.getInt("max_damage"));
-				bean.setMinDamage(rs.getInt("min_damage"));
-				bean.setIsCharge(rs.getInt("is_charge"));
-				bean.setIsExplosion(rs.getInt("is_explosion"));
-				bean.setRange(rs.getInt("range"));
-				bean.setDps(rs.getInt("dps"));
-				bean.setActualDps(rs.getInt("actual_dps"));
-				bean.setTotalDamage(rs.getInt("total_damage"));
-				bean.setInkLock(rs.getInt("ink_lock"));
-				bean.setImage(rs.getString("image"));
-				result.add(bean);
+				WeaponMasterDto dto = new WeaponMasterDto();
+				dto.setId(rs.getInt("id"));
+				dto.setName(rs.getString("weapon_master.name"));
+				dto.setType(rs.getString("weapon_type_master.name"));
+				dto.setMaxDamage(rs.getInt("max_damage"));
+				dto.setMinDamage(rs.getInt("min_damage"));
+				dto.setIsCharge(rs.getInt("is_charge"));
+				dto.setIsExplosion(rs.getInt("is_explosion"));
+				dto.setRange(rs.getInt("range"));
+				dto.setDps(rs.getInt("dps"));
+				dto.setActualDps(rs.getInt("actual_dps"));
+				dto.setTotalDamage(rs.getInt("total_damage"));
+				dto.setInkLock(rs.getInt("ink_lock"));
+				dto.setImage(rs.getString("image"));
+				result.add(dto);
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/main/java/com/example/demo/dto/WeaponQuestionDto.java
+++ src/main/java/com/example/demo/dto/WeaponQuestionDto.java
@@ -1,8 +1,8 @@
-package com.example.demo.bean;
+package com.example.demo.dto;
 
 import java.io.Serializable;
 
-public class WeaponQuestionBean implements Serializable {
+public class WeaponQuestionDto implements Serializable {
 	private Integer id;
 	private String question;
 	private String answer;
